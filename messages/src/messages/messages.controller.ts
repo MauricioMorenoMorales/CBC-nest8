d237b09981b978a9e4bdc6192a6a734f9d980d84
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2022-02-28 19:05:38 -0600

    Json Crud completed

--- messages/src/messages/messages.controller.ts
+++ messages/src/messages/messages.controller.ts
@@ -1,13 +1,27 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
-  @Get()
-  public listMessages() {}
+	messagesService: MessagesService;
+	constructor() {
+		// Service is creating its own dependencies
+		//! DONT DO THIS ON REAL APPS
+		this.messagesService = new MessagesService();
+	}
+	@Get()
+	public listMessages() {
+		return this.messagesService.findAll();
+	}
 
-  @Post()
-  public createMessage() {}
+	@Post()
+	public createMessage(@Body() body: CreateMessageDto) {
+		return this.messagesService.createOne(body.content);
+	}
 
-  @Get('/:id')
-  public getMessage() {}
+	@Get('/:id')
+	public getMessage(@Param('id') id: string) {
+		return this.messagesService.findOne(id);
+	}
 }
